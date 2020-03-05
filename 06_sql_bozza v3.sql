6c54fb54af1334ba8776907b516c21d351eeac42
Mansitos <mansitos97@gmail.com>
2020-03-05 22:26:59 +0100

    added and updated triggers on sql v3
    + created sql v4 for further updates

--- 06_sql_bozza v3.sql
+++ 06_sql_bozza v3.sql
@@ -3,6 +3,8 @@
 -- addedo unique on Esemplare.gabbia --> senza unique: più animali nella stessa gabbia....
 -- creato trigger e relativa funzione per la condizione n° 1 e 3
 -- creato trigger e relativa funzione per la condizione n° 2
+-- creato trigger e relativa funzione per la condizione n° 5
+-- creato trigger e relativa funzione per la condizione n° 4
 
 
 create table Area(
@@ -130,7 +132,7 @@ for each row
 execute procedure modifica_gabbia();
 
 create trigger aggiunta_modifica_visita -- checks n° 4
-before insert or update on Visista
+before insert or update on Visita
 for each row
 execute procedure aggiunta_modifica_visita();
 
@@ -159,20 +161,41 @@ begin
                             where    G.id = new.gabbia)) genere_ok
     where new.genere = genere_ok.genere;
 
-     if found then
-       return NEW;
+    if found then
+    	if(new.data_arrivo >= new.data_nascita) then
+		return new;
+	    end if;
+            if(TG_OP = 'UPDATE') then
+               raise exception 'ERRORE: Operazione di UPDATE non consentita! La modifica delle date ha portato a delle incongruenze! Vincolo da rispettare: la data di nascita deve essere antecedente o uguale alla data di arrivo';
+            elseif(TG_OP = 'INSERT') then
+               raise exception 'ERRORE: Operazione di INSERT non consentita! L''esemplare possiede delle incongruenze sulle date! Vincolo da rispettare: la data di nascita deve essere antecedente o uguale alla data di arrivo';
+            end if;
     end if;
-       raise exception 'Record non valido; viola il vincolo di genere! La gabbia in cui stai inserendo l"esemplare è contenuta in un abitazione a cui è stato assegnato un genere diverso da quello dell"esemplare';
+    
+    if(TG_OP = 'UPDATE') then
+        if(new.genere = old.genere) then
+            raise exception 'ERRORE: Operazione di UPDATE non consentita! La gabbia in cui si vuole spostare l''esemplare è contenuta in un abitazione il cui genere assegnato differisce da quello dell''esemplare';
+        elseif(new.gabbia = old.gabbia) then
+            raise exception 'ERRORE: Operazione di UPDATE non consentita! Il nuovo genere assegnato all''esemplare non concide con quello assegnato all''abitazione in cui è contenuta la sua gabbia';
+        end if;
+        raise exception 'ERRORE: Operazione di UPDATE non consentita! ';
+    elseif(TG_OP = 'INSERT') then
+       raise exception 'ERRORE: Operazione di INSERT non consentita! La gabbia in cui si vuole inserire l''esemplare è contenuta in un abitazione il cui genere assegnato differisce da quello dell''esemplare';
+    end if;
 
 end;
 $$ language plpgsql;
+
 
 -- 2) Alla modifica (spostamento) (UPDATE) di una gabbia in una abitazione, bisogna controllare che il genere dell'animale in essa contenuto combaci con quello assegnato alla nuova abitazione di dest.
 create or replace function modifica_gabbia() -- checks n° 2
 returns trigger
 as
 $$
-begin -- CASO 1: gabbia con un esemplare assegnato
+    -- LOGICA FUNZIONE:
+    -- Se la gabbia G1 contiene un esemplare del genere A, posso spostarla in abitazioni il cui genere assegnato è lo stesso, altrimenti violerei il vincolo di genere!
+
+begin -- CASO 1: spostamento di una gabbia con un esemplare assegnato
 
     perform *
 	from(select  E.genere  -- ottengo il genere dell'esemplare assegnato alla gabbia che sto spostando
@@ -188,7 +211,7 @@ begin -- CASO 1: gabbia con un esemplare assegnato
        return NEW;
     end if;
 	
-	begin -- CASO 2: gestione del caso in cui la gabbia da spostare è vuota, di conseguenza il CASO 1 ritornerà false su "if found" ma la gabbia può comunque essere spostata perchè è vuota!
+begin -- CASO 2: gestione del caso in cui la gabbia da spostare è vuota, di conseguenza il CASO 1 ritornerà false su "if found" ma la gabbia può comunque essere spostata perchè è vuota!
 	
     perform *
     from    Esemplare E2
@@ -197,33 +220,65 @@ begin -- CASO 1: gabbia con un esemplare assegnato
 	if not found then
 		return NEW;
 	end if;
-	 	raise exception 'Record non valido; viola il vincolo di genere! ... ';
-	end;
+	 	raise exception 'ERRORE: Operazione di UPDATE non consentita! Stai spostando una gabbia il cui esemplare contenuto appartiene ad un genere diverso di quello assegnato all''abitazione di destinazione! ';
 end;
-$$ language plpgsql;D
+end;
+$$ language plpgsql;
 
 
+-- 4) All'aggiunta (INSERT/UPDATE) di una visita bisogna controllare che data visita > data arrivo esemplare.
 create or replace function aggiunta_modifica_visita() -- checks n° 4
 returns trigger
 as
 $$
-begin
+    -- LOGICA FUNZIONE:
+    -- All'aggiunta o modifica di una visita bisogna controllare che il campo data sia coerente, ovvero che la data di visita non sia antecedente a quella di arrivo dell'esemplare nello zoo in quanto non è possibile!
+    -- Inoltre bisogna anche controllare che esista l'esemplare prima di controllarne la data
 
-    -- LOGICA FUNZIONE
+begin -- controllo prima che l'esemplare specificato dalla visita esista, poi controllerò la coerenza delle date.
 
+    perform *
+    from    Esemplare E
+    where   E.id = new.esemplare_id and E.genere = new.esemplare_gen;
+
+    if not found then
+        raise exception 'ERRORE: Operazione di INSERT/UPDATE non consentita! L''esemplare a cui fa riferimento la visita non esiste';
+    end if;
+
+begin -- una volta assicurati che l'esemplare visitato esista, controllo la coerenza delle date
+    perform *
+    from    Esemplare E
+    where   E.id = new.esemplare_id and E.genere = new.esemplare_gen and E.data_arrivo <= new.data;
+
+    if found then
+        return NEW;
+    end if;
+        raise exception 'ERRORE: Operazione di INSERT/UPDATE non consentita! Non è possibile aver visistato un esemplare prima che questo sia arrivato allo zoo!';
 end;
-$$;
+end;
+$$ language plpgsql;
 
 
+-- 5) Alla modifica di un genere assegnato (UPDATE) ad un'abitazione, bisogna controllare che non vengano violati i vincoli di genere
 create or replace function modifica_genere_abitazione() -- checks n° 5
 returns trigger
 as
 $$
+    -- LOGICA FUNZIONE:
+    -- se nell'abitazione A ci sono gabbie con esemplari di genere X, prima di cambiare il genere assegnato in Y devo spostare
+    -- questi esemplari/gabbie altrove altrimenti ad update completato avrei un'abitazione con genere assegnato X ma esemplari di genere Y al suo interno!
 begin
 
-    -- LOGICA FUNZIONE
-    -- logica-> caso 1: if nuovo genere = vecchio genere -> lascia modificare anche se non ha senso
-    --          caso 2: se ci sono gabbie nell'abitazione -> non puoi modificare!
+    perform *
+    from    Gabbia G
+    where   (G.abitazione = new.id) and (new.genere NOT IN (select E.genere
+                                                            from   Esemplare E
+                                                            where  E.gabbia = G.id));
+
+    if found then
+        raise exception 'ERRORE: Operazione di UPDATE non consentita! Non puoi cambiare genere assegnato a questa abitazione perché contiene ancora gabbie con esemplari del vecchio genere!';
+    end if;
+        return new;
 
 end;
 $$ language plpgsql;
