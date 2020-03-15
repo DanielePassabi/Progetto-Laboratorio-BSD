8cc8b8fefbfb404526d382e878b236515ca2d849
Mansitos <mansitos97@gmail.com>
2020-03-15 18:07:26 +0100

    update relazione
    + added sez. implementazione (Mansi)

--- 06_sql_bozza v6.sql
+++ 06_sql_bozza v6.sql
@@ -19,6 +19,11 @@
 -- rimosso turno di pulizia da veterinario (non serve...)
 -- creati trigger e relative funzioni per il divieto della modifica manuale degli attributi derivati (n° 4)
 -- aggiunto (causa dimenticanza) update ...gabbia... su esemplare
+-- migliorati i trigger dei due deny degli attributi derivati: aggiunto update of <...> (DA CONTROLLARE)
+
+-- CHANGELOG from v5 to v6
+-- commentato il codice dove necessario
+-- refactoring
 
 
 create table Area(
@@ -26,7 +31,7 @@ create table Area(
     numero_abitazioni   integer check(numero_abitazioni>=0) not null default 0,
 
     constraint pk_area primary key(nome)
-); -- aggiungere trigger per calcolo numero abitazioni
+);
 
 create table Genere(
     nome    varchar(32),
@@ -35,51 +40,51 @@ create table Genere(
 );
 
 create table Abitazione(
-    id              oid, -- PostgreSQL Object IDentifier type
+    id              oid, -- PostgreSQL Object IDentifier data-type
     genere          varchar(32) not null,  
     numero_gabbie   integer check(numero_gabbie>=0) not null default 0,
     area            varchar(32) not null,
 
     constraint pk_Abitazione primary key (id),
     constraint fk_genere_Abitazione_Genere foreign key (genere) references Genere(nome)
-        on delete restrict
+        on delete restrict -- non posso eliminare un genere se è ancora assegnato a qualche abitazione
         on update cascade,
     constraint fk_area_Abitazione_Area foreign key (area) references Area(nome)
-        on delete restrict
+        on delete restrict -- non posso eliminare un area se contiene ancora delle abitazioni
         on update cascade
 );
 
 create table Gabbia(
-    id          oid, -- PostgreSQL Object IDentifier type
+    id          oid, -- PostgreSQL Object IDentifier data-type
     abitazione  oid not null,
 
     constraint pk_Gabbia primary key (id),
     constraint fk_abitazione_Gabbia_Abitazione foreign key (abitazione) references Abitazione(id)
-        on delete restrict
+        on delete restrict -- non posso eliminare un'abitazione finché contiene gabbie o rimarrebbero non assegnate
         on update cascade
 );
 
 create table Esemplare(
-    id                  oid, -- PostgreSQL Object IDentifier type
+    id                  oid, -- PostgreSQL Object IDentifier data-type
     genere              varchar(32),
     nome                varchar(32) not null,
-    sesso               varchar(1) check(sesso IN ( 'F' , 'M' )) not null, --check alternative data type
+    sesso               varchar(1) check(sesso IN ( 'F' , 'M' )) not null,
     paese_provenienza   varchar(32) not null,
     data_nascita        date,
     data_arrivo         date not null,
-    gabbia              oid unique not null,
+    gabbia              oid unique not null, -- il vincolo unique garantisce che non si possano assegnare più esemplari nella stessa gabbia
 
     constraint pk_Esemplare primary key(id,genere),
     constraint fk_genere_Esemplare_Genere foreign key (genere) references Genere(nome)
-        on delete restrict
+        on delete restrict -- non posso eliminare un genere finché ho esemplari che ne fanno parte
         on update cascade,
     constraint fk_gabbia_Esemplare_Gabbia foreign key (gabbia) references Gabbia(id)
-        on delete restrict
+        on delete restrict -- non posso eliminare una gabbia se un esemplare è in essa contenuto
         on update cascade
 );
 
 create table Addetto_pulizie(
-    CF              char(16), --check alternative data type (trigger: check if CF is valid)
+    CF              char(16),
     nome            varchar(32) not null, 
     cognome         varchar(32) not null, 
     stipendio       integer check(stipendio >= 0) not null, 
@@ -91,20 +96,20 @@ create table Addetto_pulizie(
 
 create table Pulire(
     addetto_pulizie     char(16),
-    abitazione          oid,
+    abitazione          oid, -- PostgreSQL Object IDentifier data-type
 
     constraint pk_Pulire primary key (addetto_pulizie,abitazione),
     constraint fk_addetto_pulizie_Pulire_Addetto_pulizie foreign key (addetto_pulizie) references Addetto_pulizie(CF)
-        on delete cascade
+        on delete cascade -- se viene rimosso un addetto alle pulizie, rimuovo automaticamente tutte le relazioni pulire di cui faceva parte
         on update cascade,
     constraint fk_abitazione_Pulire_Abitazione foreign key (abitazione) references Abitazione(id) 
-        on delete cascade
+        on delete cascade -- se viene rimossa un'abitazione, rimuovo automaticamente tutte le relazioni pulire di cui faceva parte
         on update cascade
 );
 
 
 create table Veterinario(
-    CF              char(16), --check alternative data type (trigger: check if CF is valid)
+    CF              char(16),
     nome            varchar(32) not null, 
     cognome         varchar(32) not null, 
     stipendio       integer check(stipendio >= 0) not null, 
@@ -124,11 +129,11 @@ create table Visita(
 
     constraint pk_Visita primary key (veterinario,esemplare_id,esemplare_gen,data),
     constraint fk_veterinario_Visita_Veterinario foreign key (veterinario) references Veterinario(CF)
-        on delete restrict
+        on delete restrict -- non posso eliminare un veterinario dal DB se ho delle visite da lui effettuate nello storico
         on update cascade,
     constraint fk_esemplare_gen_Visista_Genere foreign key (esemplare_gen,esemplare_id) references Esemplare(genere,id) 
-        on delete cascade --se un esemplare muore, cancello tutte le visiste eseguite
-        on update cascade
+        on delete cascade -- se un esemplare viene rimosso, cancello tutte le visite eseguite su di esso
+        on update cascade 
 );
 
 -------> TRIGGERS <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -173,6 +178,10 @@ as
 $$
 begin
 
+-- controllo del vincolo n°1 --> controllo che il genere dell’esemplare che sto aggiungendo/spostando 
+--                               sia lo stesso dell’abitazione di destinazione (che ottengo andando a 
+--                               guardare dov’è collocata la gabbia)
+
     perform *
     from(select    A.genere -- ottengo il genere assegnato all'abitazione contenente la gabbia.
         from       Abitazione A
@@ -181,25 +190,26 @@ begin
                             where    G.id = new.gabbia)) genere_ok
     where new.genere = genere_ok.genere;
 
-    if found then
-    	if(new.data_arrivo <= new.data_nascita) then -- dopo aver controllato il vincolo 1, controlliamo il vincolo 3 (la coerenza delle date)
+    if found then -- if found == true allora posso procedere con il controllo del vincolo n°3 (sulle date)
+    	if(new.data_arrivo <= new.data_nascita) then -- controllo coerenza delle date, se il controllo risulta vero (quindi date incoerenti, lancio un'eccezione e interrompo l'op.)
             if(TG_OP = 'UPDATE') then
                raise exception 'Operazione di UPDATE non consentita! La modifica delle date ha portato a delle incongruenze! Vincolo da rispettare: la data di nascita deve essere antecedente o uguale alla data di arrivo';
             elseif(TG_OP = 'INSERT') then
                raise exception 'Operazione di INSERT non consentita! L''esemplare possiede delle incongruenze sulle date! Vincolo da rispettare: la data di nascita deve essere antecedente o uguale alla data di arrivo';
             end if;
-        end if;
+        end if; -- endif del controllo date
         
-        perform *   -- cheks n°6
+        perform *   -- se arrivo qui, le date sono coerenti e pure il vincolo di genere, continuo controllando il vincolo n°6
         from    Visita V
         where   V.esemplare_id = new.id and V.esemplare_gen = new.genere and V.data < new.data_arrivo;
 
-        if found then
+        if found then -- cerco visite la cui data è antecedente alla nuova data di arrivo, se le trovo lancio un'eccezione, altrimenti tutti i 3 controlli sono stati passati con successo: ritorno new!
             raise exception 'Operazione di UPDATE non consentita! La modifica della data di arrivo ha causato un''incongruenza: ci sono visite effettuate prima della nuova data di arrivo dell''esemplare ma non si può aver visistato un esemplare prima che questo sia arrivato nello zoo.';
         end if;
         return new;
-    end if;
+    end if; -- endif del primo controllo
     
+    -- se mi trovo qui il primo controllo (sul vincolo di genere è fallito, interrompo l'esecuzione e lancio un'eccezione)
     if(TG_OP = 'UPDATE') then
         if(new.genere = old.genere) then
             raise exception 'Operazione di UPDATE non consentita! La gabbia in cui si vuole spostare l''esemplare è contenuta in un abitazione il cui genere assegnato differisce da quello dell''esemplare';
@@ -235,21 +245,21 @@ begin -- CASO 1: spostamento di una gabbia con un esemplare assegnato
                                        from    Abitazione A
                                       where    A.id = new.abitazione));  -- Se la gabbia è vuota, bisogna poterla spostare
 
-    if found then
+    if found then -- se la gabbia è piena e l'esemplare ha lo stesso genere della nuova abitazione di dest., allora è tutto ok
        return NEW;
-    end if;
+    end if; -- se non è così, prima controllo se ci troviamo nel caso in cui la gabbia è vuota, se è vuota allora ok, se è piena allora vuol dire che sto violando il vincolo di genere!
 	
-begin -- CASO 2: gestione del caso in cui la gabbia da spostare è vuota, di conseguenza il CASO 1 ritornerà false su "if found" ma la gabbia può comunque essere spostata perchè è vuota!
-	
-    perform *
-    from    Esemplare E2
-    where   E2.gabbia = new.id;
-	
-	if not found then
-		return NEW;
-	end if;
-	 	raise exception 'Operazione di UPDATE non consentita! Stai spostando una gabbia il cui esemplare contenuto appartiene ad un genere diverso di quello assegnato all''abitazione di destinazione! ';
-end;
+    begin -- CASO 2: gestione del caso in cui la gabbia da spostare è vuota, di conseguenza il CASO 1 ritornerà false su "if found" ma la gabbia può comunque essere spostata perchè è vuota!
+    
+        perform *
+        from    Esemplare E2
+        where   E2.gabbia = new.id;
+    
+    	if not found then -- la gabbia è vuota
+    		return NEW;
+    	end if; -- la gabbia non è vuota, allora contiene un esemplare di genere errato! lancio un'eccezione
+    	 	raise exception 'Operazione di UPDATE non consentita! Stai spostando una gabbia il cui esemplare contenuto appartiene ad un genere diverso di quello assegnato all''abitazione di destinazione! ';
+    end;
 end;
 $$ language plpgsql;
 
@@ -264,31 +274,31 @@ $$
     -- Inoltre bisogna anche controllare che esista l'esemplare prima di controllarne la data
 
 begin -- controllo prima che l'esemplare specificato dalla visita esista, poi controllerò la coerenza delle date.
-
+                -- (NB: è opzionale..., lo fa già postreSQL controllando l'esistenza della chiave esterna, ma così gestiamo meglio l'errore)
     perform *
     from    Esemplare E
     where   E.id = new.esemplare_id and E.genere = new.esemplare_gen;
 
-    if not found then
+    if not found then -- eccezione nel caso in cui non esista l'esemplare (NB: è opzionale..., lo fa già postreSQL controllando l'esistenza della chiave esterna, ma così gestiamo meglio l'errore)
         raise exception 'Operazione di INSERT/UPDATE non consentita! L''esemplare a cui fa riferimento la visita non esiste';
     end if;
 
-begin -- una volta assicurati che l'esemplare visitato esista, controllo la coerenza delle date
-    perform *
-    from    Esemplare E
-    where   E.id = new.esemplare_id and E.genere = new.esemplare_gen and E.data_arrivo <= new.data;
+    begin -- una volta assicurati che l'esemplare visitato esista, controllo la coerenza delle date
+        perform *
+        from    Esemplare E
+        where   E.id = new.esemplare_id and E.genere = new.esemplare_gen and E.data_arrivo <= new.data;
 
-    if found then
-        return NEW;
-    end if;
+        if found then
+            return NEW;
+        end if;
 
-    if(TG_OP = 'UPDATE') then
-        raise exception 'Operazione di UPDATE non consentita! Non è possibile aver visistato un esemplare prima che questo sia arrivato allo zoo!';
-    elseif(TG_OP = 'INSERT') then
-        raise exception 'Operazione di INSERT non consentita! Non è possibile aver visistato un esemplare prima che questo sia arrivato allo zoo!';
-    end if;
-        
-end;
+        if(TG_OP = 'UPDATE') then
+            raise exception 'Operazione di UPDATE non consentita! Non è possibile aver visistato un esemplare prima che questo sia arrivato allo zoo!';
+        elseif(TG_OP = 'INSERT') then
+            raise exception 'Operazione di INSERT non consentita! Non è possibile aver visistato un esemplare prima che questo sia arrivato allo zoo!';
+        end if;
+
+    end;
 end;
 $$ language plpgsql;
 
@@ -304,7 +314,7 @@ $$
     -- nb: non serve il check sull'insert perchè non puoi inserire una abitaziono già con delle gabbie (non c'è rischio che queste violino il vincolo di genere perchè vengono aggiunte e controllate successivamente)
 begin
 
-    perform *
+    perform *   -- cerco degli esemplari contenuti in gabbie dell'abitazione di cui sto cambiando genere, se ne trovo, allora non posso cambiare il genere o violerei il vincolo di genere!
     from    Gabbia G
     where   (G.abitazione = new.id) and (new.genere NOT IN (select E.genere
                                                             from   Esemplare E
@@ -313,11 +323,10 @@ begin
     if found then
         raise exception 'Operazione di UPDATE non consentita! Non puoi cambiare genere assegnato a questa abitazione perché contiene ancora gabbie con esemplari del vecchio genere!';
     end if;
-        return new;
+        return new; -- l'abitazione non ha gabbie con esemplari del vechio genere, allora posso cambiarlo
 
 end;
 $$ language plpgsql;
-
 
 
 -------> DERIVED ATTRIBUTES TRIGGERS <-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -337,30 +346,35 @@ after insert or delete or update of area on Abitazione
 for each row
 execute procedure aggiorna_numero_abitazioni();
 
-create trigger deny_modifica_manuale_numero_gabbie -- triggers check  n° 4
-before update on Abitazione
+create trigger allow_modifica_nome_Area
+before update of nome on Area
+for each row
+execute procedure allow_modifica_nome_Area();
+
+create trigger deny_modifica_manuale_numero_gabbie -- triggers check  n° 4 for n_gabbie
+before update of numero_gabbie on Abitazione
 for each row
 execute procedure deny_modifica_manuale_numero_gabbie();
 
-create trigger deny_modifica_manuale_numero_abitazioni -- triggers check  n° 4
-before update on Area
+create trigger deny_modifica_manuale_numero_abitazioni -- triggers check  n° 4 for n_abitazioni
+before update of numero_abitazioni on Area
 for each row
 execute procedure deny_modifica_manuale_numero_abitazioni();
 
-create trigger set_default_numero_gabbie -- triggers check  n° 3
+create trigger set_default_numero_gabbie -- triggers check  n° 3 for n_gabbie
 before insert on Abitazione
 for each row
 execute procedure set_default_numero_gabbie();
 
-create trigger set_default_numero_abitazioni -- triggers check  n° 3
+create trigger set_default_numero_abitazioni -- triggers check  n° 3 for n_abitazioni
 before insert on Area
 for each row
 execute procedure set_default_numero_abitazioni();
 
-
 -------> DERIVED ATTRIBUTES SQL FUNCTIONS <-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-create or replace function aggiorna_numero_gabbie() -- exectues n° 1
+-- 1) All'aggiunta/spostamento/rimozione di una gabbia bisogna aggiornare l'attributo derivato n_gabbie sulle abitazioni interessate.
+create or replace function aggiorna_numero_gabbie() -- exectues n°1
 returns trigger
 as
 $$
@@ -369,6 +383,10 @@ $$
     -- eseguo la stessa operazione per la sua vecchia abitazione di appartenenza (inesistente nel caso di insert) (che sarà -1)
 begin
 
+    -- PRIMA RIGA: disattivazione temporanea del trigger che vieta le modifiche dell’attributo derivato --
+    -- è stato implementato un trigger che vieta all’utente di eseguire degli update manuali sull’attributo derivato 
+    -- per evitare che inserisca valori inconsistenti. Questo trigger va disabilitato temporaneamente durante l’aggiornamento 
+    -- (eseguito da codice, quindi non manuale) del relativo attributo derivato.
     ALTER TABLE Abitazione DISABLE TRIGGER deny_modifica_manuale_numero_gabbie;
 
     update Abitazione set numero_gabbie = (select  count(*)
@@ -388,7 +406,8 @@ begin
 end;
 $$ language plpgsql;
 
-create or replace function deny_modifica_manuale_numero_gabbie()
+-- 4) Bisogna negare la modifca manuale all'utente degli attributi Abitazione.numero_gabbie e Area.numero_abitazioni
+create or replace function deny_modifica_manuale_numero_gabbie() -- executes n°4
 returns trigger
 as
 $$
@@ -402,22 +421,25 @@ begin
 end;
 $$ language plpgsql;
 
-
-create or replace function set_default_numero_gabbie() -- exectues n° 3
+-- 3) Alla creazione di abitazioni/gabbie l'attributo derivato dovrebbe essere inizializzato a 0
+create or replace function set_default_numero_gabbie() -- exectues n°3
 returns trigger
 as
 $$
 begin
 
+if(new.numero_gabbie != 0) then
     new.numero_gabbie := 0;
     raise warning 'Il numero di gabbie è stato impostato a 0 in quanto il valore presente nella query di INSERT non era valido';
     return new;
+end if;
+return new;
 
 end;
 $$ language plpgsql;
 
-
-create or replace function aggiorna_numero_abitazioni() -- exectues n° 2
+-- 2) All'aggiunta/spostamento/rimozione di una abitazione bisogna aggiornare l'attributo derivato n_abitazioni sulle aree interessate.
+create or replace function aggiorna_numero_abitazioni() -- exectues n°2
 returns trigger
 as
 $$
@@ -426,6 +448,10 @@ $$
     -- eseguo la stessa operazione per la sua vecchia area di appartenenza (inesistente nel caso di insert) (che sarà -1)
 begin
 
+    -- PRIMA RIGA: disattivazione temporanea del trigger che vieta le modifiche dell’attributo derivato --
+    -- è stato implementato un trigger che vieta all’utente di eseguire degli update manuali sull’attributo derivato 
+    -- per evitare che inserisca valori inconsistenti. Questo trigger va disabilitato temporaneamente durante l’aggiornamento 
+    -- (eseguito da codice, quindi non manuale) del relativo attributo derivato.
     ALTER TABLE Area DISABLE TRIGGER deny_modifica_manuale_numero_abitazioni;
 
     update Area set numero_abitazioni = (select  count(*)
@@ -446,7 +472,8 @@ end;
 $$ language plpgsql;
 
 
-create or replace function deny_modifica_manuale_numero_abitazioni()
+-- 4) Bisogna negare la modifca manuale all'utente degli attributi Abitazione.numero_gabbie e Area.numero_abitazioni
+create or replace function deny_modifica_manuale_numero_abitazioni() -- executes n°4
 returns trigger
 as
 $$
@@ -460,16 +487,19 @@ begin
 end;
 $$ language plpgsql;
 
-
-create or replace function set_default_numero_abitazioni() -- exectues n° 3
+-- 3) Alla creazione di abitazioni/gabbie l'attributo derivato dovrebbe essere inizializzato a 0
+create or replace function set_default_numero_abitazioni() -- exectues n°3
 returns trigger
 as
 $$
 begin
 
+if(new.numero_abitazioni != 0) then
     new.numero_abitazioni := 0;
     raise warning 'Il numero di abitazioni è stato impostato a 0 in quanto il valore presente nella query di INSERT non era valido';
     return new;
+end if;
+return new;
 
 end;
 $$ language plpgsql;
